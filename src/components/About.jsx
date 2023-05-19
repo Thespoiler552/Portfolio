9979f2f8c884c4e191e5a14982d8704a65e36c7a
barry_t <barry_t@etna-alternance.net>
2023-05-19 16:40:12 +0200

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Compétences.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+       Je suis un étudiant en troisième année de bachelor Administration des systèmes d'infrastructures sécurisées avec des compétences en cybersécurité.
+        Je suis passionné par la protection des systèmes et des données sensibles. J'ai acquis une solide compréhension des principes de sécurité informatique, 
+       y compris la gestion des risques, la prévention des attaques et la protection des informations confidentielles.
+       je suis également compétent en langages de Scripting (Python, Bash, Powershell) et en développement de logiciels (C, C++, JavaScript, Typescript). 
+       Je suis un apprenant rapide et je suis prêt à mettre mes compétences en cybersécurité et en développement de logiciels à votre service pour concrétiser vos idées. Travaillons ensemble pour donner vie à vos projets !
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,254 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "A propos",
+  },
+  {
+    id: "work",
+    title: "Experiences",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Réseau et sécurité",
+    icon: web,
+  },
+  {
+    title: "Pentesting",
+    icon: mobile,
+  },
+  {
+    title: "Linux",
+    icon: backend,
+  },
+  {
+    title: "Programmation",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Technicien Support Informatique",
+    company_name: "Ecobank",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "March 2021 - April 2022",
+    points: [
+      "Administrateur Windows server 2016 (Active directory)",
+      "Support des utilisateurs",
+      "Gestion du parc Informatique (Imprimantes, +50 PC...)",
+      "Maintenance Informatique(panne, problème réseau, installation logiciel…)",
+      "Utilisation de suite Office ",
+      "Gestion des sauvegardes",
+      "Migration de poste vers windows 11"
+    ],
+  },
+  {
+    title: "Bachelor 3 Administration d'infrastructures securisées",
+    company_name: "ETNA",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Mar 2023 - Aujourd'hui",
+    points: [
+      "Piscine de 6 semaines : Développement d’une  API REST en C# avec Mongodb.",
+      "Administration Système sous Windows et LINUX (Debian, Docker, Active Directory, Bash…).",
+      "Développement de son portfolio React, Treejs, Tailwind.",
+      "Développement en C, Typescript, Python, Bash.",
+    ],
+  },
+ 
+  {
+    title: "Bachelor Informatique",
+    company_name: "Int'ech",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Sep 2022 - Fev 2023",
+    points: [
+      "Création d'un jeux-video mini pokemon en JavaScript Node.js Html Css",
+      "Projet de Formation humaine : Création d'une scène de théatre Inspiré du film Scarface.",
+   ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Je crois fermement que ma passion pour le numérique et ma soif constante d'apprendre en font un choix idéal pour contribuer efficacement à votre équipe de cybersécurité.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "J'ai constamment cherché à en apprendre davantage sur les dernières avancées dans le domaine de la cybersécurité, des systèmes informatiques et des nouvelles technologies émergentes.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "je crois fermement que la cybersécurité nécessite une approche collaborative. Travailler en équipe et partager mes connaissances avec mes collègues est une valeur que je chéris",
+    name: "",
+    designation: "",
+    company: "",
+    image: "",
+  },
+];
+
+const projects = [
+  {
+    name: "Identification des vulnérabilités de sécurité",
+    description:
+      "Utilisation d'outils spécialisés tels que Maltego, Recon-ng, et SpiderFoot pour collecter et analyser des informations à partir de sources publiques et identifier les vulnérabilités de sécurité critiques, Évaluation des risques de sécurité pour l'entreprise et proposition de recommandations.",
+    tags: [
+      {
+        name: "maltego",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Recon-ng",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Spider-foot",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Code To Work",
+    description:
+      "Conception d'une API REST permettant de créer et gérer des utilisateurs ainsi que des produits, Déploiement et configuration automatisés d'une VM AWS avec Terraform, installation de Git et Docker, et exécution d'un projet via Terraform sur la VM déployée, Mise en place et déploiement de l'API et de la base de données sous Docker, Creation du Front-end associé.",
+    tags: [
+      {
+        name: "C#, Angular",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Docker",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Configuration d'un serveur de fichiers",
+    description:
+      "J'ai analysé les besoins de l'entreprise, installé et configuré le  serveur de fichiers, et créé des comptes utilisateurs et des partages de  fichiers avec des autorisations d'accès personnalisées. Le projet a été  validé avec succès, et j'ai assuré la formation des utilisateurs et le  support technique.",
+    tags: [
+      {
+        name: "Wireshark",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Active Directory",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Tcp",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
